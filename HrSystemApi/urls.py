fd3eb7d87bd9a37f46cbcb3e5e68916ea111f6f8
Ahmed Gomaa <73650922+ahmed-Gomaa18@users.noreply.github.com>
2022-06-16 08:21:55 +0200

    Update urls.py

--- HrSystemApi/urls.py
+++ HrSystemApi/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
     path('register/', views.register),
     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
 
-    path('check-in/employee-<str:employee_pk>/', views.check_in, name="check_in"),
-    path('check-out/employee-<str:employee_pk>/attendance-<str:attendance_pk>/', views.check_out, name="check_out"),
-    path('list-attendances/employee-<str:employee_pk>/', views.list_attendances, name="list_attendances"),
+    path('check-in/', views.check_in, name="check_in"),
+    path('check-out/attendance-<str:attendance_pk>/', views.check_out, name="check_out"),
+    path('list-attendances/', views.list_attendances, name="list_attendances"),
     path('list-attendances-all/', views.list_attendances_all, name="list_attendances_all"),
 
-    path('check-in-again/employee-<str:employee_pk>/attendance-<str:attendance_pk>/', views.check_in_again, name="check_in_again"),
-    path('check-out-again/employee-<str:employee_pk>/attendance-<str:attendance_pk>/', views.check_out_again, name="check_out_again"),
+    path('check-in-again/attendance-<str:attendance_pk>/', views.check_in_again, name="check_in_again"),
+    path('check-out-again/attendance-<str:attendance_pk>/', views.check_out_again, name="check_out_again"),
 
 ]
